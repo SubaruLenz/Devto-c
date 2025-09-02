256f4e0c43d6e6dbb51c09d79449cb425f26bb37
NguyenTrungKien <51208515+SubaruLenz@users.noreply.github.com>
2025-09-02 08:16:57 +0000

    feat: added google authentication

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+//import GitHubProvider from "next-auth/providers/github";
 
 import { db } from "~/server/db";
 
@@ -32,7 +33,8 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    GoogleProvider,
+    //GitHubProvider,
     /**
      * ...add more providers here.
      *
